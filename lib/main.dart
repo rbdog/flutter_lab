6af974e31223792e1fd319fa6db6b23265471343
Rubydog <Rubydog725@gmail.com>
2023-07-05 00:56:59 +0900

    add firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_lab/firebase_options.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  const app = MyApp();
+  runApp(app);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: const Text('データをSet'),
+              onPressed: () async {
+                const collection = 'users';
+                const doc = 'abcd1234';
+                const user = {'name': 'Rubydog', 'age': 22};
+                await FirebaseFirestore.instance
+                    .collection(collection)
+                    .doc(doc)
+                    .set(user);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
